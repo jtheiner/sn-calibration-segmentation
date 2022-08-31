eaab847f8d5a8428fe2ca90cd647c8fe5abef963
jtheiner <jonas.theiner@tib.eu>
2022-08-31 10:25:53 +0000

    all required variants for segment localization

---
+++ run.sh
@@ -0,0 +1,25 @@
+
+OUTPUT_DIR="/nfs/home/theinerj/vid2pos_git-tib-other/cvsports22_challenge/sn-calib-challenge/data/segment_localization"
+
+python custom_extremities.py -p $OUTPUT_DIR --checkpoint ../resources/train_59.pt --split valid --num_points_lines 2 --num_points_circles 8
+python custom_extremities.py -p $OUTPUT_DIR --checkpoint ../resources/train_59.pt --split test --num_points_lines 2 --num_points_circles 8
+python custom_extremities.py -p $OUTPUT_DIR --checkpoint ../resources/train_59.pt --split wc14-test --num_points_lines 2 --num_points_circles 8
+python custom_extremities.py -p $OUTPUT_DIR --checkpoint ../resources/train_59.pt --split valid --num_points_lines 4 --num_points_circles 8
+python custom_extremities.py -p $OUTPUT_DIR --checkpoint ../resources/train_59.pt --split test --num_points_lines 4 --num_points_circles 8
+python custom_extremities.py -p $OUTPUT_DIR --checkpoint ../resources/train_59.pt --split wc14-test --num_points_lines 4 --num_points_circles 8
+
+# for DLT from line baseline -> best results for two major points
+# num_points_circles does not matter
+
+# ResNet-101 retrained
+python custom_extremities.py -p $OUTPUT_DIR --checkpoint ../resources/train_59.pt --split wc14-test --pp_num_points 2 --pp_maxdists 40
+python custom_extremities.py -p $OUTPUT_DIR --checkpoint ../resources/train_59.pt --split test --pp_num_points 2 --pp_maxdists 40
+
+# ResNet-50 original
+python -m src.baseline_extremities --soccernet /nfs/data/soccernet/calibration/ --split wc14-test -p $OUTPUT_DIR/sn-baseline --masks true
+python -m src.baseline_extremities --soccernet /nfs/data/soccernet/calibration/ --split test -p $OUTPUT_DIR/sn-baseline
+
+
+
+
+
